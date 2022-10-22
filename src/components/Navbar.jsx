5dbf8a69455448eba5ced71ccc66128aa9183cee
Divyanshu Prasad <90151736+Divyanshu9822@users.noreply.github.com>
2022-10-22 13:59:28 +0530

    upload commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,10 +8,10 @@ const Navbar = () => {
     <header className="p-3 text-bg-dark sticky-top">
       <div className="container">
         <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-          <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+          <Link to="/ReactJS-E-commerce" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
             {/* <svg className="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg> */}
             <h4 className=" mx-3">Savana</h4>
-          </a>
+          </Link>
 
           <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
             {/* <li><Link to="/" className="nav-link px-2 text-white">Home</Link></li> */}
